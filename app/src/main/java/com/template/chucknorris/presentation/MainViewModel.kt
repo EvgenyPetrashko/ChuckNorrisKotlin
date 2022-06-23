0457d7fd5c480a70b4b62094f40b3af2316b0a45
epetrashko <Lopory_324!>
2022-06-23 14:45:33 +0300

    ViewModel dependency were added and MainViewModel was implemented

---
+++ app/src/main/java/com/template/chucknorris/presentation/MainViewModel.kt
@@ -0,0 +1,41 @@
+package com.template.chucknorris.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.template.chucknorris.domain.entity.JokeWrapper
+import com.template.chucknorris.domain.usecase.GetJokeUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor(private val getJokeUseCase: GetJokeUseCase) : ViewModel() {
+    private val jokeMutable = MutableLiveData<String?>(null)
+    val joke: LiveData<String?>
+        get() = jokeMutable
+
+    private val errorMutable = MutableLiveData<String?>(null)
+    val error: LiveData<String?>
+        get() = errorMutable
+
+    private val loadingMutable = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean>
+        get() = loadingMutable
+
+    fun loadJoke() {
+        loadingMutable.postValue(true)
+        viewModelScope.launch {
+            when (val jokeWrapper = getJokeUseCase.execute()) {
+                is JokeWrapper.Joke -> {
+                    jokeMutable.postValue(jokeWrapper.value)
+                    errorMutable.postValue(null)
+                }
+                is JokeWrapper.Error -> {
+                    errorMutable.postValue(jokeWrapper.error)
+                    jokeMutable.postValue(null)
+                }
+            }
+            loadingMutable.postValue(false)
+        }
+    }
+}
