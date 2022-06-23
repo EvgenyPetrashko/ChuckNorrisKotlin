e5c603f563131f73afaa891ec3f66c2a6684a53b
epetrashko <Lopory_324!>
2022-06-23 13:24:23 +0300

    RxJava removed from data layer

--- app/src/main/java/com/template/chucknorris/data/JokeApi.kt
+++ app/src/main/java/com/template/chucknorris/data/JokeApi.kt
@@ -1,10 +1,9 @@
 package com.template.chucknorris.data
 
 import com.template.chucknorris.data.entities.JokeFromServer
-import io.reactivex.Single
 import retrofit2.http.GET
 
 interface JokeApi {
     @GET("random")
-    fun randomJoke(): Single<JokeFromServer?>?
+    suspend fun randomJoke(): JokeFromServer
 }
